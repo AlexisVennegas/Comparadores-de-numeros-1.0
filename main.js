0de293ca0249caa620a8854ec06d201a0be4ac3e
Alexis Venegas <107916070+AlexVG101@users.noreply.github.com>
2022-07-01 12:54:01 +0200

    JS
    11/05/2022

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("hola huevos")
+
+
+var nombre = "alexis venegas";
+var altura = "175";
+var concatenacion = nombre + " " + altura
+
+document.write(nombre)
+document.write(altura) 
+
+var datos = document.getElementById("datos")
